4a653d1441bf8dd4236b8b7955723dba328ae3c3
Zafuzi <zacharyfoutz@gmail.com>
2017-03-14 00:14:54 -0700

    Initial Commit

---
+++ CoffeeSnobs/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeSnobs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Learn a little more about what is CoffeeSnobs.com";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Discover how to get in touch with the folks at CoffeeSnobs.com";
+
+            return View();
+        }
+    }
+}
